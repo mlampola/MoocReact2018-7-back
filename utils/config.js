41b7246992cea7aa815b6e12a2b78fb79289f696
Markus Lampola <markus.lampola@kolumbus.fi>
2020-10-29 22:43:30 +0200

    Fix MongoDB URL

--- utils/config.js
+++ utils/config.js
@@ -7,7 +7,7 @@ let port = process.env.PORT
 // Production: MONGO_DB = ds253783.mlab.com:53783/persons
 // Development (Prod?): MONGO_DB = markus-db.7jvzk.mongodb.net/markus-db?retryWrites=true&w=majority
 // Test: MONGO_DB = markus-test.glz0k.mongodb.net/markus-test?retryWrites=true&w=majority
-let mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DB}`
+let mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_DB}`
 
 if (process.env.NODE_ENV === 'test') {
   port = process.env.TEST_PORT
